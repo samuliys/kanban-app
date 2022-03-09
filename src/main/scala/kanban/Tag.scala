e255d2e82a47161540884fd0a2b9b446087939c5
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-09 20:35:08 +0200

    Add basic features to Tag class

--- src/main/scala/kanban/Tag.scala
+++ src/main/scala/kanban/Tag.scala
@@ -1,5 +1,12 @@
 package kanban
 
-class Tag(private val name: String) {
+import scala.collection.mutable.Buffer
 
+class Tag(private val name: String) {
+  private val cards = Buffer[Card]()
+
+  def addCard(card: Card) = cards += card
+  def removeCard(card: Card) = cards.remove(cards.indexOf(card))
+
+  def getTagCards = cards
 }
