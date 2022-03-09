848a31fd0c04314b6b8571fc6b33e0b8fe792c46
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-09 19:00:00 +0200

    Add buttons for adding new columns and editing columns

--- src/main/scala/kanban/Board.scala
+++ src/main/scala/kanban/Board.scala
@@ -1,21 +1,22 @@
 package kanban
 
+import scalafx.scene.paint.Color
+
 import scala.collection.mutable.Buffer
 
 
 class Board(private var name: String) {
   private val columns = Buffer[Column]()
-  this.addColumn("Test1")
-  this.addColumn("Test2")
+  this.addColumn("Column1", Color.OrangeRed)
+  this.addColumn("Test2", Color.SeaGreen)
 
   def getColumns = this.columns
   def getName = name
 
   def rename(newName: String) = name = newName
 
-  def addColumn(name: String = "untitled") = {
-    val column = new Column(name)
-    //column.addCard(name)
+  def addColumn(name: String = "untitled", color: Color = Color.Blue) = {
+    val column = new Column(name, color)
     columns += column
   }
 }
