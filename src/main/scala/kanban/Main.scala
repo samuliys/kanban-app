614d19619d81ba2a7d579b0a0710cdbfd6606c3c
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-09 13:18:53 +0200

    Add basic card creation using class instances

--- src/main/scala/kanban/Main.scala
+++ src/main/scala/kanban/Main.scala
@@ -1,10 +1,15 @@
 package kanban
 
+import javafx.scene.paint.Color
 import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.geometry.Pos.Center
 import scalafx.scene.Scene
-import scalafx.scene.layout.Pane
-import scalafx.scene.control.Button
+import scalafx.scene.layout._
+import scalafx.scene.control._
+import scalafx.scene.text.{Font, TextAlignment}
+
+import scala.collection.mutable.Buffer
 
 
 object Main extends JFXApp {
@@ -13,16 +18,32 @@ object Main extends JFXApp {
     width = 600
     height = 450
   }
+  var number = 3
 
-  val root = new Pane
+  val kanbanApp = new Kanban()
+  val fileManager = new FileHandler
 
-  val button = new Button("I'm a button!")
+  var activeCard = kanbanApp.getBoards.getColumns.head.getCards.head
+  var cardActiveStatus = true
 
-  button.onAction = (event) => {
-      println("Kanban!")
+  def drawCard(card: Card): VBox = new VBox(4) {
+    border = new Border(new BorderStroke(Color.SKYBLUE, BorderStrokeStyle.Solid, new CornerRadii(2), new BorderWidths(6)))
+    maxWidth = 250
+    alignment = Center
+    children += new Label(card.getText) {
+      wrapText = true
+      textAlignment = TextAlignment.Center
+      font = Font.font("arial", 16)
+    }
   }
 
-  root.children += button
+  def root: VBox = new VBox(8) {
+
+    for (card <- kanbanApp.getBoards.getColumns.head.getCards) {
+      children += drawCard(card)
+    }
+  }
+
   val scene = new Scene(root)
   stage.scene = scene
 }
