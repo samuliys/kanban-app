55552cbe615edbe836e55558083651fa245d18f9
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-09 18:57:05 +0200

    Add support for column color

--- src/main/scala/kanban/Column.scala
+++ src/main/scala/kanban/Column.scala
@@ -5,7 +5,7 @@ import scalafx.scene.paint.Color
 import scala.collection.mutable.Buffer
 
 
-class Column(private var name: String) {
+class Column(private var name: String, private var color: Color) {
   private val cards = Buffer[Card]()
   this.addCard("card1", Color.Blue)
   this.addCard("card2", Color.GreenYellow)
@@ -13,11 +13,17 @@ class Column(private var name: String) {
 
   def getCards = this.cards
   def getName = name
+  def getColor = color
 
   def rename(newName: String) = name = newName
 
   def addCard(name: String = "untitled", color: Color = Color.Blue) = {
     val card = new Card(name, color)
     cards += card
+  }
+
+  def editColumn(newName: String, newColor: Color) = {
+    name = newName
+    color = newColor
   }
 }
