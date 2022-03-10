0b98c847e7b525f1f5e9b4ec5350787f81371dbe
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-10 17:34:46 +0200

    Add support for tags

--- src/main/scala/kanban/Card.scala
+++ src/main/scala/kanban/Card.scala
@@ -2,13 +2,20 @@ package kanban
 
 import scalafx.scene.paint.Color
 
+import scala.collection.mutable.Buffer
+
 class Card(private var text: String, private var color: Color) {
 
+  private val tags = Buffer[Tag]()
   def getText = text
   def getColor = color
+
+  def getTags = tags
 
   def editCard(newText: String, newColor: Color) = {
     text = newText
     color = newColor
   }
+
+  def addTag(tag: Tag) = tags += tag
 }
