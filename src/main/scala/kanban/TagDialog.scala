262cf42f77de88a51418ebd76cb2f78955ff0338
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-10 15:36:27 +0200

    Add ability to add and remove tags using dialog

--- src/main/scala/kanban/TagDialog.scala
+++ src/main/scala/kanban/TagDialog.scala
@@ -1,12 +1,13 @@
 package kanban
 
-import kanban.Main.stage
+import kanban.Main.{kanbanApp, stage}
 import scalafx.Includes._
 import scalafx.application.Platform
-import scalafx.geometry.Insets
+import scalafx.collections.ObservableBuffer
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout._
 import scalafx.scene.control._
+import scalafx.scene.paint.Color
 
 
 object TagDialog {
@@ -18,44 +19,91 @@ object TagDialog {
   }
 
   val okButtonType = new ButtonType("OK", ButtonData.OKDone)
-  dialog.dialogPane().buttonTypes = Seq(okButtonType, ButtonType.Cancel)
+  dialog.dialogPane().buttonTypes = Seq(ButtonType.OK)
 
   val tagText = new TextField() {
     promptText = "Tag Name"
   }
 
   val addTagButton = new Button("Add Tag") {
+    disable = true
     onAction = (event) => {
-      println(tagText.text())
+      kanbanApp.addTag(tagText.text())
       tagText.text = ""
+      errorLabel.text = ""
+      tagRemoveMenu.items = ObservableBuffer(getTagList)
     }
   }
-  val tagRemoveMenu = new MenuButton("Select Tag to Remove") {
 
+  val errorLabel = new Label {
+    textFill = Color.Red
+  }
+  def getTagList = kanbanApp.getTagNames.toList
 
+  val tagRemoveMenu: ComboBox[String] = new ComboBox(getTagList) {
+    promptText = "Select Tag to Remove"
+    onAction = (event) => {
+      deleteTagButton.disable = false
+    }
   }
 
-  val grid = new GridPane() {
-    hgap = 10
-    vgap = 10
-    padding = Insets(20, 200, 10, 10)
 
-    add(new Label("New Tag:"), 0, 0)
-    add(tagText, 1, 0)
-    add(addTagButton, 2, 0)
-    add(new Label("Remove Tag:"), 0, 1)
-    add(tagRemoveMenu, 1, 1)
+
+  val deleteTagButton = new Button("Delete Tag") {
+    disable = true
+    onAction = (event) => {
+      kanbanApp.removeTag(tagRemoveMenu.value())
+      tagRemoveMenu.items = ObservableBuffer(getTagList)
+      tagRemoveMenu.promptText = "Select Tag to Remove"
+      disable = true
+    }
+  }
+
+
+  def drawContents = new VBox(10) {
+    minWidth = 500
+    children += new HBox(10) {
+      children += new Label("New Tag:")
+      children += tagText
+      children += addTagButton
+      children += errorLabel
+    }
+    children += new HBox(5) {
+      children += new Label("Remove Tag:")
+      children += tagRemoveMenu
+      children += deleteTagButton
+    }
   }
 
   val okButton = dialog.dialogPane().lookupButton(okButtonType)
 
-  dialog.dialogPane().content = grid
+  tagText.text.onChange { (_, _, newValue) =>
+    if (newValue == "") {
+      errorLabel.text = "Tag name can't be empty"
+      addTagButton.disable = true
+    } else if (newValue.length > 10) {
+      errorLabel.text = "Tag name too long"
+      addTagButton.disable = true
+    } else if (getTagList.contains(newValue)) {
+      errorLabel.text = "Tag name can't be samuli"
+      addTagButton.disable = true
+    } else {
+      errorLabel.text = ""
+      addTagButton.disable = false
+    }
+  }
+
+  dialog.dialogPane().content = drawContents
 
   Platform.runLater(tagText.requestFocus())
 
   def reset() = {
     dialog.title = "Kanban - Tags"
-    dialog.headerText = "ManageTags"
+    dialog.headerText = "Manage Tags"
     tagText.text = ""
+    errorLabel.text = ""
+    dialog.dialogPane().content = drawContents
+
+
   }
 }
