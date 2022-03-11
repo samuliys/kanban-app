38d1cdfecc79b0a477dac6f316c7a10ccc00e78b
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-11 14:15:18 +0200

    Add methods for displaying deadline data

--- src/main/scala/kanban/Deadline.scala
+++ src/main/scala/kanban/Deadline.scala
@@ -1,12 +1,27 @@
 package kanban
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
 class Deadline(private val date: LocalDate) {
   def getRawDate = date
 
-  def getDate = date.toString
+  def getDate = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
 
   def daysUntil = ChronoUnit.DAYS.between(LocalDate.now(), date)
+
+  def daysUntilString = daysUntil.toString
+
+  def getString = {
+    if (daysUntil == 1) {
+      s"Due tomorrow ($getDate)"
+    } else if (daysUntil > 0) {
+      s"Due in $daysUntilString days ($getDate)"
+    } else if (daysUntil == 0) {
+      "Due today"
+    } else {
+      "Due day ago"
+    }
+  }
 }
