e662613767a00866e31a74e8cd3976f19ac616b9
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-03-12 16:37:58 +0200

    Change column creation so that columns must have unique names

--- src/main/scala/kanban/ColumnDialog.scala
+++ src/main/scala/kanban/ColumnDialog.scala
@@ -1,10 +1,9 @@
 package kanban
 
 import scalafx.scene.paint.Color
-import kanban.Main.{activeBoard, activeColumn, columnEditActive, kanbanApp, stage}
+import kanban.Main.{activeBoard, activeColumn, columnEditActive, stage}
 import scalafx.Includes._
 import scalafx.application.Platform
-import scalafx.geometry.Insets
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout._
 import scalafx.scene.control._
@@ -30,25 +29,42 @@ object ColumnDialog {
     promptText = "Color"
   }
 
-  val grid = new GridPane() {
-    hgap = 10
-    vgap = 10
-    padding = Insets(20, 200, 10, 10)
+  val errorLabel = new Label {
+    textFill = Color.Red
+  }
 
-    add(new Label("Name:"), 0, 0)
-    add(columnName, 1, 0)
-    add(new Label("Color:"), 0, 1)
-    add(columnColor, 1, 1)
+  def drawContents = new VBox(10) {
+    minWidth = 500
+    children += new HBox(10) {
+      children += new Label("Name:")
+      children += columnName
+      children += errorLabel
+    }
+    children += new HBox(10) {
+      children += new Label("Color:")
+      children += columnColor
+    }
   }
 
   val okButton = dialog.dialogPane().lookupButton(okButtonType)
   okButton.disable = true
 
   columnName.text.onChange { (_, _, newValue) =>
-    okButton.disable = newValue.trim().isEmpty
+
+    if (activeBoard.getColumns.map(_.getName).contains(newValue)) {
+      okButton.disable = true
+      errorLabel.text = "List with that name already exists"
+    } else if (newValue.trim().isEmpty) {
+      okButton.disable = true
+    } else {
+      errorLabel.text = ""
+      okButton.disable = false
+    }
+
+
   }
 
-  dialog.dialogPane().content = grid
+  dialog.dialogPane().content = drawContents
 
   Platform.runLater(columnName.requestFocus())
 
