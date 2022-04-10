e436f6d01a9223eae4170b70b5065ce192d56106
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-04-10 17:33:44 +0300

    Add package object for settings

---
+++ src/main/scala/kanban/settings.scala
@@ -0,0 +1,10 @@
+import scalafx.scene.text.Font
+
+package object settings {
+  val CardHeight = 120
+  val CardWidth = 310
+  val ColumnWidth = 330
+
+  val DefaultFont = Font.font("arial", 13)
+  val CardTextFont = Font.font("arial", 15)
+}
