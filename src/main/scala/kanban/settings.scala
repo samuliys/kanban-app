8f337e76842ec0c65dcd4694ad1d550e39c53e6b
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-04-16 18:10:41 +0300

    Add new settings

--- src/main/scala/kanban/settings.scala
+++ src/main/scala/kanban/settings.scala
@@ -2,15 +2,24 @@ import scalafx.scene.text.Font
 import scalafx.scene.paint.Color
 
 package object settings {
+
+  // Starting window dimensions
+  val StartingWidth = 1100
+  val StartingHeight = 800
+
+  // UI element dimensions
   val CardHeight = 120
   val CardWidth = 310
   val ColumnWidth = 330
 
+  // Defualt fonts
   val DefaultFont = Font.font("arial", 13)
   val CardTextFont = Font.font("arial", 15)
 
+  // Max amounts of boards and columns
   val MaxBoards = 5
   val MaxColumns = 7
 
+  // Default colors
   val DefaultColor = Color.Black
 }
