4c21b925ca56288dbf4f63630e531907e69f6c37
“Samuli <samuli.yli-salomaki@aalto.fi>
2022-04-16 18:37:35 +0300

    Add showcase templates

--- src/main/scala/kanban/CardTemplates.scala
+++ src/main/scala/kanban/CardTemplates.scala
@@ -5,11 +5,11 @@ import scalafx.scene.paint.Color
 import java.time.LocalDate
 import scala.collection.mutable.Buffer
 
-object Template1 extends Card("Joku otsikko", Color.LightBlue, Buffer[String](), new Checklist, Some(new Deadline(LocalDate.of(2022, 4, 27)))) {
-  getChecklist.addTask("Do Something")
-  getChecklist.addTask("Do Smthng Else")
+// Premade templates for showcase, the user can create their own
+object Template1 extends Card("Example 1", Color.Black, Color.LightBlue, Buffer[String](), new Checklist, Some(new Deadline(LocalDate.of(2022, 4, 27)))) {
+  getChecklist.addTask("Create a new list")
+  getChecklist.addTask("Add new cards")
+  getChecklist.addTask("Create a new board")
 }
 
-object Template2 extends Card {
-
-}
+object Template2 extends Card("Example 2", Color.Red, Color.Orange, Buffer[String](), new Checklist, Some(new Deadline(LocalDate.now())), None, None, Some("https://www.aalto.fi/fi"))
